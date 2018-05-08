2008fce03b380fb68aa3943dbf4a8c0c7a1ca7ed
Simão <spcl_vnf@hotmail.com>
2018-05-08 22:39:55 +0100

    Change the main store getters from index to a getters file

--- store/getters.js
+++ store/getters.js
@@ -1,1 +1,17 @@
-export default {}
+export default {
+  getTotalSpending: state => {
+    const value = state.payments.reduce(
+      (acc, payment) => acc + payment.price / payment.period,
+      0
+    );
+    return parseFloat(value).toFixed(2);
+  },
+  getFrequency: state => value => {
+    const frequency = state.periods.find(p => p.value === value);
+    return frequency && frequency.label ? frequency.label : '';
+  },
+  getFrequencyValue: state => label => {
+    const frequency = state.periods.find(p => p.label === label);
+    return frequency.value;
+  }
+}
--- store/index.js
+++ store/index.js
@@ -2,6 +2,7 @@ import Vuex from 'vuex';
 import uuidv4 from 'uuid/v4';
 import localStorage from '~/plugins/localStorage';
 import state from './state';
+import getters from './getters';
 
 const createStore = () => {
   return new Vuex.Store({
@@ -21,24 +22,7 @@ const createStore = () => {
         localStorage.set(state.payments);
       }
     },
-    getters: {
-      getTotalSpending: state => {
-        const value = state.payments.reduce(
-          (acc, payment) => acc + payment.price / payment.period,
-          0
-        );
-
-        return parseFloat(value).toFixed(2);
-      },
-      getFrequency: state => value => {
-        const frequency = state.periods.find(p => p.value === value);
-        return frequency && frequency.label ? frequency.label : '';
-      },
-      getFrequencyValue: state => label => {
-        const frequency = state.periods.find(p => p.label === label);
-        return frequency.value;
-      }
-    }
+    getters
   });
 };
 
